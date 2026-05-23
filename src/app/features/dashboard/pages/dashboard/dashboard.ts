b372f17368c13d7fa585781cb80baab31541fe9f
ikshit1 <ikshit1@gmail.com>
2026-05-23 18:49:50 +0530

    Sidebar changes

--- src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -4,11 +4,20 @@ import { Sidebar } from '../../../../layout/sidebar/sidebar';
 import { DashboardCard } from '../../../../shared/dashboard-card/dashboard-card';
 import { ChartWidget } from '../../../../shared/chart-widget/chart-widget';
 import { TransactionsTable } from '../../../../shared/transactions-table/transactions-table';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [Navbar, Sidebar, DashboardCard, ChartWidget, TransactionsTable],
+  imports: [Navbar, Sidebar, DashboardCard, ChartWidget, TransactionsTable, CommonModule
+    
+  ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {}
+export class Dashboard {
+  isSidebarOpen = false;
+
+  toggleSidebar(): void {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+}
--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
-export class Navbar {}
+export class Navbar {
+  @Output() menuClick = new EventEmitter<void>();
+
+  toggleMenu(): void {
+    this.menuClick.emit();
+  }
+}
